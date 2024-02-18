0361d7724c554c7a1bd92186eddc4e7ec146e3a4
N0Algorithm <akshay312004@gmail.com>
2024-02-18 14:01:07 +0530

    Debugged Social Tree

--- clone/components/SocialTree.js
+++ clone/components/SocialTree.js
@@ -16,19 +16,19 @@ const SocialTree = ({social}) => {
                 <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://facebook.com/${facebook}`}>
                     <img className='w-6' src="/facebook.svg"/>
                 </Link>
-                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://instagram.com/${facebook}`}>
+                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://instagram.com/${instagram}`}>
                     <img className='w-6' src="/instagram.svg"/>
                 </Link>
-                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://youtube.com/${facebook}`}>
+                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://youtube.com/${youtube}`}>
                     <img className='w-6' src="/youtube.svg"/>
                 </Link>
-                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://linkedin.com/${facebook}`}>
+                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://linkedin.com/${linkedin}`}>
                     <img className='w-6' src="/linkedin.svg"/>
                 </Link>
-                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://github.com/${facebook}`}>
+                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://github.com/${github}`}>
                     <img className='w-6' src="/github.svg"/>
                 </Link>
-                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://twitter.com/${facebook}`}>
+                <Link className='bg-white rounded-full p-2 hover:bg-zinc-100 transition-all duration-500 hover:scale-110 shadow border border-gray-70 mx-1 select-none' target='_blank' href={`https://twitter.com/${twitter}`}>
                     <img className='w-6' src="/twitter.svg"/>
                 </Link>
             </div>
